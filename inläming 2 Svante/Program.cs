7e66da150fdcf158e19f4fe0f73574b2345a31a0
Svante Joelsson <svante.joelsson@gmail.com>
2020-12-03 12:13:14 +0100

    NU börjar vi

---
+++ inläming 2 Svante/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace inläming_2_Svante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
