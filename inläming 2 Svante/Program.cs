a0e67dd037fcc89b6861062434d37538bf455ca5
Svante Joelsson <svante.joelsson@gmail.com>
2020-12-03 12:13:34 +0100

    början

--- inläming 2 Svante/Program.cs
+++ inläming 2 Svante/Program.cs
@@ -6,7 +6,17 @@ namespace inläming_2_Svante
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("\n\t\t\tVälkommen!\n\nVar god skriv in den hemliga koden");
+            Console.Write(">");
+            string password = Console.ReadLine();
+            password.ToUpper();
+
+            if (password == "COFFE N CODE")
+            {
+                Console.WriteLine("Du är värdig");
+            }
+            else //hoppar måsvingar för jag är ett proffs!
+                Console.WriteLine("Du är inte värdig");
         }
     }
 }
